aab0881364b18aa478ca2da841f576220969bffd
Rahul <rahulbehera2011@gmail.com>
2021-05-03 19:04:13 +0530

    Added changes for cognito integration

---
+++ src/app/user-info-api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import auth from './auth';
+
+
+export default{  
+    getUserInfo(){
+        var jwtToken = auth.auth.getSignInUserSession().getAccessToken().jwtToken;
+        const USERINFO_URL = 'https://'+auth.auth.getAppWebDomain() + '/oauth2/userInfo';
+        var requestData = {
+            headers: {
+                'Authorization': 'Bearer '+ jwtToken
+            }
+        }
+        console.log('USERINFO_URL',USERINFO_URL);
+        return axios.get(USERINFO_URL, requestData).then(response => { 
+            return response.data;
+        });
+    }
+}
